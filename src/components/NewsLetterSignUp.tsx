472b462b3161875df3163450e3a5b0748242ae18
Mike <mikemd833@gmail.com>
2024-07-17 12:37:24 -0400

    Installed MUI component package, and applied skeleton of NewsLetter component.

---
+++ src/components/NewsLetterSignUp.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+const NewsLetterSignUp = () => {
+    return (
+        <Card>
+        <CardContent>
+          <Typography>
+            Hello World!
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+}
+
+export default NewsLetterSignUp;
