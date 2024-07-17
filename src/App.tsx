0062c23c987995ba852e9cad42b0bf8b55f5a135
Mike <mikemd833@gmail.com>
2024-07-17 18:08:47 -0400

    Added Success Card Component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import { useState } from 'react';
-import NewsLetterSignUp from './components/NewsLetterSignUp'
+import NewsLetterSignUp from './components/NewsLetterSignUp';
+import NewsLetterSuccess from './components/NewsLetterSuccess';
 import './App.css'
 
 function App() {
   const [isNewsletterSignedUp, setNewsletterSignUp] = useState(false);
+  const [isNewsletterSuccess, setNewsletterSuccess] = useState(false);
   
   const handleEmailSignUp = (email) => {
     setNewsletterSignUp(!isNewsletterSignedUp);
-    console.log(email);
-  }
+    setNewsletterSuccess(true);
+  };
+
+  const handleNewsLetterSuccess = () => {
+    setNewsletterSuccess(false);
+  };
 
   return (
     <>
       {!isNewsletterSignedUp &&
         <div className='news-letter-sign-up'>
           <NewsLetterSignUp handleEmailSignUp={handleEmailSignUp} />
+        </div>
+      }
+      {isNewsletterSuccess &&
+        <div className='news-letter-sign-up'>
+          <NewsLetterSuccess handleNewsLetterSuccess={handleNewsLetterSuccess} />
         </div>
       }
     </>
---
+++ src/components/NewsLetterSuccess.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+interface NewsLetterSuccessProps {
+	handleNewsLetterSuccess: () => void;
+}
+
+const NewsLetterSuccess: React.FC<NewsLetterSuccessProps> = (props) => {
+	const { handleNewsLetterSuccess } = props;
+	const [timer, setTimer] = useState(3);
+
+	useEffect(() => {
+		const intTimer = setInterval(() => {
+			setTimer(timer - 1);
+		}, 1000)
+		if(timer === 0) {
+			handleNewsLetterSuccess();
+			return clearInterval(intTimer);
+		}
+	}, [timer, handleNewsLetterSuccess])
+
+	return (
+		<Card style={{ borderRadius: '10px', backgroundColor: '#00c394' }}>
+			<CardContent>
+				<Typography>Thank you for signing up!</Typography>
+			</CardContent>
+		</Card>
+	);
+}
+
+export default NewsLetterSuccess;
