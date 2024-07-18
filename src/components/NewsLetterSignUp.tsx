c8c90e7174f03090c55883be72819e4f88d91256
Mike <mikemd833@gmail.com>
2024-07-18 11:43:02 -0400

    Added loading when sending email.

--- src/components/NewsLetterSignUp.tsx
+++ src/components/NewsLetterSignUp.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import CircularProgress from '@mui/material/CircularProgress';
 import IconButton from '@mui/material/IconButton';
 
 interface NewsLetterSignUpProps {
@@ -16,6 +17,20 @@ const NewsLetterSignUp: React.FC<NewsLetterSignUpProps> = (props) => {
 	const { handleEmailSignUp, handleNewsLetterClose } = props;
 	const [email, setEmail] = useState('');
   const [emailInvalid, setEmailInvalid] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [send, setSend] = useState(false);
+
+  useEffect(() => {
+    if(send !== false) {
+      const loading = setInterval(() => {
+        handleEmailSignUp(email);
+        setIsLoading(false);
+        setEmail('');
+        setSend(false);
+      }, 1500)
+      return () => {clearInterval(loading)}
+    }
+  }, [email, isLoading, send, handleEmailSignUp])
 
 	const handleEmailText = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setEmail(event.target.value);
@@ -27,7 +42,8 @@ const NewsLetterSignUp: React.FC<NewsLetterSignUpProps> = (props) => {
     const isEmailValid = emailRegex.test(email);
     if(isEmailValid) {
       setEmailInvalid(false);
-		handleEmailSignUp(email);
+      setIsLoading(true);
+      setSend(true);
     } else {
       setEmailInvalid(true);
     }
@@ -47,7 +63,14 @@ const NewsLetterSignUp: React.FC<NewsLetterSignUpProps> = (props) => {
 				<TextField className="news-letter-sign-up-card-textfield" variant="outlined" placeholder="example@boast.com" value={email} onChange={handleEmailText} />
         {emailInvalid && <Typography className="news-letter-sign-up-card-error">Invalid Email</Typography>}
 			</CardContent>
-			<Button className="news-letter-sign-up-card-button" variant="outlined" onClick={handleSignUp}><Typography>Sign Up</Typography></Button>
+      {!isLoading &&
+        <Button className="news-letter-sign-up-card-button" variant="outlined" onClick={handleSignUp}>
+          <Typography>Sign Up</Typography>
+        </Button>
+      }
+      {isLoading &&
+        <CircularProgress style={{ color: "#00c394", marginBottom: "10px" }}/>
+      }
 		</Card>
 	);
 }
