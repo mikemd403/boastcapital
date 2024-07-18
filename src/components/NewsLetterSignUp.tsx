a1ffd6caf22f8628cfb5ffea98968184394bc452
Mike <mikemd833@gmail.com>
2024-07-18 11:04:38 -0400

    Added Chevron close.

--- src/components/NewsLetterSignUp.tsx
+++ src/components/NewsLetterSignUp.tsx
@@ -4,6 +4,8 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import IconButton from '@mui/material/IconButton';
 
 interface NewsLetterSignUpProps {
 	handleEmailSignUp: (email: string) => void;
@@ -32,10 +34,15 @@ const NewsLetterSignUp: React.FC<NewsLetterSignUpProps> = (props) => {
 
 	return (
 		<Card className="news-letter-sign-up-card">
-			<CardContent>
-				<Typography className="news-letter-sign-up-card-header">
-					Boast Newsletter
-				</Typography>
+			<CardContent className="news-letter-sign-up-card-content">
+        <div className="news-letter-sign-up-card-header">
+          <Typography >
+            Boast Newsletter
+          </Typography>
+          <IconButton style={{ marginLeft: '5px' }}>
+            <KeyboardArrowDownIcon style={{ fill: '#031417' }}  />
+          </IconButton>
+        </div>
 				<TextField className="news-letter-sign-up-card-textfield" variant="outlined" placeholder="example@boast.com" value={email} onChange={handleEmailText} />
         {emailInvalid && <Typography className="news-letter-sign-up-card-error">Invalid Email</Typography>}
 			</CardContent>
